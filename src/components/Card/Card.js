57958edd918ecf05e95cf0bc73e74cb58aae1b5b
Marcin Kapturski <marcin.kapturski@newvoicemedia.com>
2020-06-19 13:49:33 +0200

    Card Status: css styles for status of run and tests count

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import cardIconPng from '../../layout/icons/report_icon.png';
+import Status from '../../components/Status/Status';
 
 const CardWrapper = styled.div`
     display: flex;
-    padding: 20px;
+    padding: 0 0 20px 0;
     margin: 40px 40px 0 0;
     width: 170px;
     height: 170px;
     justify-content: center;
-    align-items: center;
+    align-items: top;
     float: left;
     font-size: 13px;
     text-decoration: none;
@@ -30,6 +31,30 @@ const CardWrapper = styled.div`
         text-decoration: none !important;
         color: ${({theme}) => theme.colors.pinkdlackdash};
     }
+
+    .cardtop {
+    display: top;
+    width: 100px;
+    height: 20px;
+    background-color: ${({theme}) => theme.colors.pinkdlackdash};
+    border: none;
+    margin: 0;
+    padding: 0px;
+    text-align: center;
+    vertical-align: middle;
+    }
+`;
+
+const CardTop = styled.div`
+    display: block top;
+    width: 55px;
+    height: 20px;
+    background-color: ${({theme}) => theme.colors.graydark}; 
+    border-radius: 20px;
+    margin: 3px 3px 17px 110px;
+    padding: 0px;
+    text-align: center;
+    vertical-align: right;
 `;
 
 const CardIcon = styled.div`
@@ -39,7 +64,18 @@ const CardIcon = styled.div`
     background-image: url(${cardIconPng});
     background-repeat: no-repeat;
     border: none;
-    margin: 0 0 10px 40px;
+    margin: 40px 0 10px 75px;
+    padding: 0px;
+    text-align: center;
+    vertical-align: middle;
+`;
+
+const CardText = styled.div`
+    display: block;
+    width: 20px;
+    height: 21px;
+    border: none;
+    margin: 0 0 10px 35px;
     padding: 0px;
     text-align: center;
     vertical-align: middle;
@@ -54,14 +90,15 @@ class Card extends Component {
         // const reportName = title.replace(/[a-zA-Z]+/g, '')
 
         return (
-            <CardWrapper>     
+            <CardWrapper>
                 <div className="Card">
-                    <CardIcon></CardIcon>
-                    <Link to="/report">{ title.substr(7, 16) }</Link>
+                    <CardTop class="carttop"><Status></Status></CardTop>
+                    <CardIcon class="cartIcon"></CardIcon>
+                    <CardText><Link to="/report">{ title.substr(7, 16) }</Link></CardText>
                 </div>
             </CardWrapper>
           )
     }
-} 
+}
 
 export default Card;
---
+++ src/components/Status/Status.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const TestsCount = styled.div`
+        float: left;
+        padding: 5px;
+        margin: 0;          
+        color: ${({theme}) => theme.colors.gray};
+        font-size: 10px;
+`;
+
+const TestRunFailed = styled.div`
+        float: right;        
+        width: 10px;
+        height: 10px;
+        padding: 0;
+        margin: 5px 5px 5px 0;
+        border-radius: 5px;     
+        color: ${({theme}) => theme.colors.grey};
+        background-color: ${({theme}) => theme.colors.red};
+        font-size: 8px;
+        text-decoration: none !important;
+        justify-content: center;
+        align-items: center;
+`;
+
+const TestRunPassed = styled.div`
+        float: right;        
+        width: 10px;
+        height: 10px;
+        padding: 0;
+        margin: 5px 5px 5px 0;
+        border-radius: 5px;     
+        color: ${({theme}) => theme.colors.grey};
+        background-color: ${({theme}) => theme.colors.green};
+        font-size: 8px;
+        text-decoration: none !important;
+        justify-content: center;
+        align-items: center;
+`;
+
+class Status extends Component {
+
+    render() {
+        
+        const { title } = this.props;
+
+        return (
+            <div>
+               { this.props.title > 0 
+                    ? <>
+                      <TestsCount>14/30</TestsCount>
+                            <TestRunFailed></TestRunFailed>
+                      </>
+                      : <>
+                        <TestsCount>14/30</TestsCount>
+                            <TestRunPassed></TestRunPassed>
+                        </> }
+            </div>
+          )
+    }
+}
+
+export default Status;
